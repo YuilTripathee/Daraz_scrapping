600e037043a3e2eafdb6c7913699cd579c3ec43b
Yuil Tripathee <yuiltripathee79@gmail.com>
2018-05-02 15:41:05 +0545

    Add iteration

--- main.py
+++ main.py
@@ -9,35 +9,30 @@ class Scraper:
         self.r = requests.get(url)
         self.my_soup = soup(self.r.text, 'html.parser')
         self.containers = self.my_soup.find_all('div', {'class' : ['sku', '-gallery']})
-        # self.product = self.containers[2] # testing
-
-    def make_soup(self):
-        return self.containers
+        self.product = self.containers[13]
+          
+    def run(self, json_data):
+        for item in self.containers:
+            if item.find_all("div")[1].find_all("span")[0]['class'] == ['sale-flag-percent']:
+                if index_finder(json_data, self.scrap_sku(item)):
+                    print('Item already on index')
+                    pass
+                else:
+                    dict_to_add = {
+                        'sku' : self.scrap_sku(item)
+                    }
+                    json_data.append(dict_to_add)
+                    print('Item not in index')
+                    pass
+                
+            else:
+                print('No discount here')
             
+        
     def scrap_sku(self, item):
         self.prod_sku = item['data-sku']
-        print(self.prod_sku)
         return self.prod_sku
-
-    # def discount_classifier(self):
-    #     for item in self.containers:
-    #         if item.find_all("div")[1].find_all("span")[0]['class'] == ['sale-flag-percent']:
-    #             print('Discount here')
-    #             pass
-            
-    # def scrap_full(self):
-    #     if self.product.find_all("div")[1].find_all("span")[0]['class'] == ['sale-flag-percent']:
-    #         self.prod_cont = self.product.a.find_all("div")
-    #         self.product_name = self.product.a.h2.text.strip().replace(',',' ')
-    #         self.product_hash = self.product['data-sku']
-    #         self.product_link = self.product.a['href']
-    #         self.product_image_link = self.prod_cont[0].noscript.img['src']
-    #         self.product_discount = self.prod_cont[1].find_all("span")[0].text
-    #         self.price_mini = self.prod_cont[1].find_all("span")[1]
-    #         self.product_price = self.price_mini.span.find_all("span")[1]['data-price']
-    #         print(self.product_price)
-    #     else:
-    #         print('No discount on: ', self.product_name)
+    
 
 class SearchSort:
     # sorts the data making SKU as index
@@ -66,7 +61,7 @@ class SearchSort:
             else:
                 lo = mid + 1
         return False
-
+    
 # function to find if product from scrap is already present and returns its index in array
 def index_finder(json_list, sku_data):
     search = SearchSort()
@@ -75,24 +70,25 @@ def index_finder(json_list, sku_data):
 
 if __name__ == '__main__': 
     scrap = Scraper('https://www.daraz.com.np/earphones-headsets/')
-    sku_web = scrap.scrap_sku()
-    # scraped_items = scrap.scrap_full()
-    for item in scrap.make_soup():
 
-        pass
+    # data collection
     with open('database/dataset.json', 'r') as fp:
         data_file = json.load(fp)
+    # configuration text file for id
     with open('database/id_config.txt', 'r') as fp:
         last_id = int(fp.read())
-    index_rank = index_finder(data_file, sku_web)
-    if index_rank:
-        print('Product already present and price is constant, no need to change')
-    else:
-        print('This product is not in our database. It should be added.')
-        dict_to_add = {
-            'sku' : sku_web
-        }
-        data_file.append(dict_to_add)
+    
+    scrap.run(data_file)
+    
+    # index_rank = index_finder(data_file)
+    # if index_rank:   
+    #     print('Product already present and price is constant, no need to change')
+    # else:
+    #     print('This product is not in our database. It should be added.')
+    #     dict_to_add = {
+    #         'sku' : sku_web
+    #     }
+    #     data_file.append(dict_to_add)
     
     # write to the file finally
     with open('database/dataset.json', 'w') as fp:
