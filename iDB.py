6afee77d8ca2667a7dee42002c4a4cd2cb200598
Yuil Tripathee <yuiltripathee79@gmail.com>
2018-05-12 23:29:45 +0545

    add iDB.py to directly send query to database

---
+++ iDB.py
@@ -0,0 +1,18 @@
+import json
+import pymysql
+if __name__ == '__main__':
+    # extract database server configuration
+    with open('database/DBconf.json') as fp:
+        dbConf = json.load(fp)
+        fp.close()
+
+    # start a connection
+    db = pymysql.connect(dbConf['server'], dbConf['username'], dbConf['password'], dbConf['database'])
+    # make a cursor to work on database (to execute queries)
+    cursor = db.cursor()
+    print('Connection established with database.')
+    
+    # work on queries
+    while True:
+        command = input('> ')
+        cursor.execute(command)
