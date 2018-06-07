da6081de9c5551b7e21a0b84d48fbf09deb9f14e
Yuil Tripathee <yuiltripathee79@gmail.com>
2018-06-07 17:47:38 +0545

    Updated test result with latest one (using memory cache)

---
+++ test/run-test.sh
@@ -0,0 +1,6 @@
+get the list of file and folder in this category
+    if results folder is present, delete it with all its content
+    if jmeter.jtl file is present, delete it 
+get jmeter.sh from another directory in this computer
+expost bin folder to PATH folder of bash shell
+run the test command
